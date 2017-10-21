35a492090081ad212a6980116003596717f601a8
ryo-1 <f1242022@kgu.jp>
2017-10-22 02:03:24 +0900

    meke変更

--- apple_makedata.py
+++ apple_makedata.py
@@ -1,10 +1,10 @@
-import os, glob
+import glob
 import numpy as np
 from sklearn import cross_validation
 from keras.preprocessing.image import load_img, img_to_array
 
 root_dir = "./image/"
-categories = ["red_apple", "green_apple"]
+categories = ["one", "two", "three"]
 nb_classes = len(categories)
 image_size = 32
 
@@ -24,5 +24,5 @@ Y = np.array(Y)
 X_train, X_test, y_train, y_test = \
     cross_validation.train_test_split(X, Y)
 xy = (X_train, X_test, y_train, y_test)
-np.save("./image/apple.npy", xy)
+np.save("./image/three_check.npy", xy)
 print("ok,", len(Y))
